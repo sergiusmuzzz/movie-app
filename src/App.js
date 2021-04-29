dfc18e225472d95f2ebeed324b7308222334f06f
Sergey <sergiorichie@gmail.com>
2021-04-29 12:09:44 -0700

    Prepared API calls.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const API_KEY = 'c7c5d1ce6c96e2785bed26f92732a5cc';
+
+const App = () => {
+
+    const [initialized, setInitialized] = useState(false);
+    const [movieList, setMovieList] = useState([]);
+
+
+    // API calls to returns JSON that's added to the state
+    const fetchData = async () => {
+        const movies = await axios(
+            `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US`,
+        );
+
+        const getDetails = id => {
+            return axios
+                .get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US&append_to_response=release_dates`)
+                .then(response => {
+                    return response.data;
+                });
+        };
+
+        const filteredMovies = await Promise.all(movies.data.results.map(async movie => {
+            movie.details = await getDetails(movie.id);
+            return movie;
+        }));
+
+        console.log(filteredMovies);
+        setMovieList(filteredMovies);
+    };
+
+    //most popular movies titles based on average ratings
+
+    useEffect(() => {
+        // Call the API only once
+        if (!initialized) {
+            fetchData();
+            setInitialized(true);
+        }
+    }, [initialized]);
+    return (
+        <div className="App">
+            {console.log(movieList)}
+        </div>
+    );
 }
 
 export default App;
